b9d2be73a0e81b0a07d5ec4837a68f085c33b0d7
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-08-08 09:15:20 +0200

    gocrypto: symmetric encryption, AES with GCM

---
+++ symmetric_encryption/AES_example.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+func toByteArray(s string) []byte {
+	bytes := make([]byte, len(s))
+
+	for i := 0; i < len(bytes); i++ {
+		bytes[i] = byte(s[i])
+	}
+
+	return bytes
+}
+
+func EncryptWithGCMBlockMode(data []byte, key []byte) ([]byte, error) {
+
+	//Initialize the block cipher
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	//Generate a randomized nonce
+	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
+	nonce := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	nonce, _ = hex.DecodeString("bbaa99887766554433221100") //For comparing with the Java code
+
+	//Choose a block cipher mode
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	return aesgcm.Seal(nil, nonce, data, nil), nil
+
+}
+
+func DecryptWithGCMBlockMode(ciphertext []byte, key []byte) ([]byte, error) {
+	//Initialize the block cipher
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	//The same nonce as before (we'll consider a parameter instead)
+	nonce, _ := hex.DecodeString("bbaa99887766554433221100") //For comparing with the Java code
+
+	//Choose a block cipher mode
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	return aesgcm.Open(nil, nonce, ciphertext, nil)
+
+}
+
+func main() {
+	key, _ := hex.DecodeString("000102030405060708090a0b0c0d0e0f")
+	plaintext := toByteArray("hello, world!")
+	fmt.Printf("plain: %s\n", hex.EncodeToString(plaintext))
+
+	ciphertext, err := EncryptWithGCMBlockMode(plaintext, key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("encrypted: %x\n", ciphertext)
+
+	decryptedtext, err := DecryptWithGCMBlockMode(ciphertext, key)
+
+	fmt.Printf("decrypted: %x\n", decryptedtext)
+	fmt.Printf("%s\n", string(decryptedtext))
+
+}
