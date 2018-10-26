c9d96463abc0fe7223166a9edf21df2f5c0a38c0
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-10-26 19:36:45 +0200

    gocrypto: HASH large file

--- hash/hash_files.go
+++ hash/hash_files.go
@@ -6,12 +6,13 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
-const minSizeLargeFile = 1048576
+const minSizeLargeFile = 10000 //1048576
 
 func hashSmallFile(filename string) {
 	fmt.Printf("HASH small file %s\n\n", filename)
@@ -29,7 +30,36 @@ func hashSmallFile(filename string) {
 }
 
 func hashLargeFile(filename string) {
-	fmt.Println("HASH LARGE file", filename)
+	fmt.Printf("HASH LARGE file %s\n\n", filename)
+
+	// Open file for reading
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Create new hasher, which is a writer interface
+	hasher := md5.New()
+
+	// Default buffer size for copying is 32*1024 or 32kb per copy
+	// Use io.CopyBuffer() if you want to specify the buffer to use
+	// It will write 32kb at a time to the digest/hash until EOF
+	// The hasher implements a Write() function making it satisfy
+	// the writer interface. The Write() function performs the digest
+	// at the time the data is copied/written to it. It digests
+	// and processes the hash one chunk at a time as it is received.
+	_, err = io.Copy(hasher, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Now get the final sum or checksum.
+	// We pass nil to the Sum() function because
+	// we already copied the bytes via the Copy to the
+	// writer interface and don't need to pass any new bytes
+	checksum := hasher.Sum(nil)
+	fmt.Printf("Md5 checksum: %x\n", checksum)
 }
 
 func HashFile(filename string) {
